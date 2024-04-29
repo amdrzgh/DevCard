ebba25a3b18416d7d040bb32d0e1d4090fcdcbc4
siavash <amdrzgh@gmail.com>
2024-04-29 15:03:19 +0430

    project section from index moved to partial view projects

---
+++ DevCard/DevCard_MVC/Models/project.cs
@@ -0,0 +1,11 @@
+﻿namespace DevCard_MVC.Models
+{
+    public class project
+    {
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string  DEscription { get; set; }
+        public string Client { get; set; }
+    }
+}
